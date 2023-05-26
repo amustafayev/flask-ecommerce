ecc409d61a8ef01b8fe8dbc1775fc2cef69f8ec5
amustafayev <afgan.mustafayev@pashabank.az>
2023-05-27 02:53:20 +0400

    Implement favorite functionality

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+
 # from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -9,16 +11,16 @@ app = Flask(__name__)
 login_manager = LoginManager(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
-
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 app.app_context().push()
 bcrypt = Bcrypt(app)
-# login_manager.login_view = "login_page"
-# login_manager.login_message_category = "info"
-# from market import routes
+
+from src import admin
 
 from src import routers
